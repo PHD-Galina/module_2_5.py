db20d9490485a3673de7a8cdcd943903c1730bac
PHD-Galina <dr.galinka.1@gmail.com>
2024-12-25 11:24:10 +0300

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -1,10 +1,12 @@
-def get_matrix (n, m, value) :
+def get_matrix(n, m, value):
     matrix = []
+
     for i in range(n):
         matrix.append([])
         for j in range(m):
-                matrix[i].append(value)
-        return matrix
+            matrix[i].append(value)
+
+    return matrix
 
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
